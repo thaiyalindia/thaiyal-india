12b751cdd7a351efd96dd6a713b4f88dcbbc8866
thaiyalindia <thaiyalindia@gmail.com>
2026-06-29 09:17:20 +0530

    Update Icons.tsx

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 import logoImage from '../assets/logo.png';
 
-export function ThaiyalLogo({ className }: { className?: string }) {
-  return <Image src={logoImage} alt="Thaiyal India" className={className} />;
+export function ThaiyalLogo({ className }: { className?: string }) 
+{
+  return <Image 
+           src={logoImage} 
+           alt="Thaiyal India" 
+           className={className} />;
 }
